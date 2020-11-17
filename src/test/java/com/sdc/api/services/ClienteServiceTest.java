9f6aed2bfd00a7e258704dc83d6d19deeafe6367
wildfireY <luiz06lhap@gmail.com>
2020-11-17 10:39:19 -0300

    Testes ClienteService e RegraService

---
+++ src/test/java/com/sdc/api/services/ClienteServiceTest.java
@@ -0,0 +1,85 @@
+package com.sdc.api.services;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sdc.api.entities.Cliente;
+import com.sdc.api.entities.Usuario;
+import com.sdc.api.repositories.ClienteRepository;
+import com.sdc.api.repositories.UsuarioRepository;
+import com.sdc.api.utils.ConsistenciaException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class ClienteServiceTest {
+	
+	//IMCOMPLETO
+	//IMCOMPLETO
+	//IMCOMPLETO
+	
+	@MockBean
+	private ClienteRepository clienteRepository;
+	
+	@MockBean
+	private UsuarioRepository usuarioRepository;
+
+	@Autowired
+	private ClienteService clienteService;
+
+	@Test
+	public void testBuscarPorId() throws ConsistenciaException {
+
+		BDDMockito.given(clienteRepository.findById(Mockito.anyInt())).willReturn(Optional.of(new Cliente()));
+
+		Optional<Cliente> resultado = clienteService.buscarPorId(1);
+
+		assertTrue(resultado.isPresent());
+
+	}
+
+	@Test(expected = ConsistenciaException.class)
+	public void testBuscarPorIdNaoExistente() throws ConsistenciaException {
+
+		clienteService.buscarPorId(-2);
+
+	}
+
+	@Test
+	public void testBuscarPorRg() throws ConsistenciaException {
+		BDDMockito.given(clienteRepository.findByRg(Mockito.anyString())).willReturn(Optional.of(new Cliente()));
+
+		Optional<Cliente> resultado = clienteService.buscarPorRg("99930632");
+
+		assertTrue(resultado.isPresent());
+	}
+
+	@Test
+	public void testSalvar() throws ConsistenciaException {
+		
+		Cliente cliente = new Cliente();
+		Usuario usuario = new Usuario();
+		usuario.setId(1);
+		cliente.setUsuario(usuario);
+		
+		BDDMockito.given(usuarioRepository.findById(Mockito.anyInt())).willReturn(Optional.of(new Usuario()));
+		
+		BDDMockito.given(clienteRepository.save(Mockito.any(Cliente.class))).willReturn(new Cliente());
+
+		Cliente resultado = clienteService.salvar(cliente);
+
+		assertNotNull(resultado);
+	}
+}
---
+++ src/test/java/com/sdc/api/services/RegraServiceTest.java
@@ -0,0 +1,90 @@
+package com.sdc.api.services;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sdc.api.utils.ConsistenciaException;
+import com.sdc.api.entities.Regra;
+import com.sdc.api.repositories.RegraRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class RegraServiceTest {
+
+	@MockBean
+	private RegraRepository regraRepository;
+
+	@Autowired
+	private RegraService regraService;
+
+	@Test
+	public void testBuscarPorIdExistente() throws ConsistenciaException {
+
+		BDDMockito.given(regraRepository.findById(Mockito.anyInt())).willReturn(Optional.of(new Regra()));
+
+		Optional<Regra> resultado = regraService.buscarPorId(1);
+
+		assertTrue(resultado.isPresent());
+	}
+
+	@Test(expected = ConsistenciaException.class)
+	public void testBuscarPorIdNaoExistente() throws ConsistenciaException {
+
+		BDDMockito.given(regraRepository.findById(Mockito.anyInt())).willReturn(Optional.empty());
+
+		regraService.buscarPorId(1);
+	}
+
+	@Test
+	public void testBuscarPorNomeExistente() throws ConsistenciaException {
+
+		BDDMockito.given(regraRepository.findByNome(Mockito.anyString())).willReturn(new Regra());
+
+		Optional<Regra> resultado = regraService.buscarPorNome("Luizão");
+
+		assertTrue(resultado.isPresent());
+	}
+	
+	@Test(expected = ConsistenciaException.class)
+	public void testBuscarPorNomeNaoExistente() throws ConsistenciaException {
+
+		BDDMockito.given(regraRepository.findByNome(Mockito.anyString())).willReturn(null);
+
+		regraService.buscarPorNome("Godofredo");
+	}
+	
+	@Test
+	public void testBuscarTodasRegras() throws ConsistenciaException{
+
+		List<Regra> lstRegra = new ArrayList<Regra>();
+		lstRegra.add(new Regra());
+
+		BDDMockito.given(regraRepository.findAll()).willReturn((lstRegra));
+
+		Optional<List<Regra>> resultado = regraService.buscarTodasAsRegras();
+
+		assertTrue(resultado.isPresent());
+	}
+	@Test(expected = ConsistenciaException.class)
+	public void testBuscarTodasRegrasNaoEncontrado() throws ConsistenciaException{
+
+		BDDMockito.given(regraRepository.findAll()).willReturn((null));
+
+		regraService.buscarTodasAsRegras();
+
+	}
+}
