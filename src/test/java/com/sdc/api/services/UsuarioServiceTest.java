7c571d9e21f8dc985f74fed95766a8dbf02717bb
Luan Marques <luanfelipe988@gmail.com>
2020-11-16 21:29:13 -0300

    Merge branch testes into master

---
+++ src/test/java/com/sdc/api/services/UsuarioServiceTest.java
@@ -0,0 +1,34 @@
+package com.sdc.api.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UsuarioServiceTest {
+
+	@Test
+	void testBuscarPorId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBuscarPorCpf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testVerificarCredenciais() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSalvar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAlterarSenhaUsuario() {
+		fail("Not yet implemented");
+	}
+
+}
